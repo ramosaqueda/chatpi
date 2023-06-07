646bafde0b0b5447bec728884e2efb16932392b2
ramosaqueda <ramos.aqueda@gmail.com>
2023-06-07 17:34:02 -0400

    security

--- src/helpers/errors.ts
+++ src/helpers/errors.ts
@@ -1,6 +1,14 @@
 import { Request, Response } from 'express'
+import { IError } from 'src/modules/user/interfaces/helpers/ierror'
 export default class {
 	static notFound(req: Request, res: Response): void {
 		res.status(404).send('Ruta no encontrada')
 	}
+
+	static genericError(error: IError, _req: Request, res: Response): void {
+		res.status(error.status || 500).json({
+		  message: error.message,
+		  stack: error.stack,
+		})
+	 }
 }
